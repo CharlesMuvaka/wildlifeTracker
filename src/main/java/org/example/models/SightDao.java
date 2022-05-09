ec1dd583aaff5784955fb6c2ddc4cd5e4f7f6fb7
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-09 23:36:25 +0300

    add methods to insert and retrive data in the sighting table

---
+++ src/main/java/org/example/models/SightDao.java
@@ -0,0 +1,45 @@
+package org.example.models;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+
+
+public class SightDao implements SightingInterface{
+    @Override
+    public void addSighting(Sighting sight) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO sightings (sightanimalname,sightanimaltype,rangername ,locationname ,sightanimalage,sightanimalhealth,  sightingtime ) VALUES (:sightAnimalName, :sightAnimalType,  :rangerName,:locationName, :sightAnimalAge, :sightAnimalHealth, now() )";
+            sight.sightingId = (int) con.createQuery(sql, true)
+                    .addParameter("sightAnimalName", sight.sightAnimalName)
+                    .addParameter("sightAnimalType", sight.sightAnimalType)
+                    .addParameter("rangerName", sight.rangerName)
+                    .addParameter("locationName", sight.locationName)
+                    .addParameter("sightAnimalHealth", sight.sightAnimalHealth)
+                    .addParameter("sightAnimalAge", sight.sightAnimalAge)
+                    .executeUpdate()
+                    .getKey();
+        }catch (Sql2oException ex) {
+            System.out.println(ex); //"oops "we have an error!
+        }
+    }
+
+    @Override
+    public Sighting getSightingById(int id) {
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery("SELECT * FROM sightings WHERE sightingid = :sightingId")
+                    .addParameter("sightingId", id) //key/value pair, key must match above
+                    .executeAndFetchFirst(Sighting.class); //fetch an individual item
+        }
+    }
+
+    @Override
+    public List<Sighting> getAllSightings() {
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery("SELECT * FROM sightings") //raw sql
+                    .executeAndFetch(Sighting.class); //fetch a list
+        }
+    }
+}
