34c67c983fe2ef22d919de96bfd067107a0dd489
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-09 23:49:34 +0300

    fix:remove redudant access modifiers

--- src/main/java/org/example/models/RareInterface.java
+++ src/main/java/org/example/models/RareInterface.java
@@ -1,4 +1,13 @@
 package org.example.models;
 
+import java.util.List;
+
 public interface RareInterface {
+     void addAnimal(RareAnimal animal);
+
+    //Read
+     RareAnimal getAnimalById(int id);
+     List<RareAnimal> allAnimals();
+
+    List<RareAnimal> rareAnimals();
 }
