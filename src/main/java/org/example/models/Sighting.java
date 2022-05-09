707a7b26125dc1edaa0d7d43c4573fa27623035c
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-09 23:33:59 +0300

    add sighting blue print with getters and setters

---
+++ src/main/java/org/example/models/Sighting.java
@@ -0,0 +1,105 @@
+package org.example.models;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.util.Objects;
+
+public class Sighting {
+    public int sightingId;
+    public String sightAnimalName;
+    public String sightAnimalType;
+    public String locationName;
+    public String rangerName;
+    public String sightAnimalAge;
+    public String sightAnimalHealth;
+    public Timestamp sightingTime;
+
+
+    public Sighting( String sightAnimalName, String sightAnimalType, String locationName, String rangerName, String sightAnimalAge, String sightAnimalHealth) {
+        this.sightingId = sightingId;
+        this.sightAnimalName = sightAnimalName;
+        this.sightAnimalType = sightAnimalType;
+        this.locationName = locationName;
+        this.rangerName = rangerName;
+        this.sightAnimalAge = sightAnimalAge;
+        this.sightAnimalHealth = sightAnimalHealth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Sighting)) return false;
+        Sighting sighting = (Sighting) o;
+        return sightingId == sighting.sightingId && Objects.equals(sightAnimalName, sighting.sightAnimalName) && Objects.equals(sightAnimalType, sighting.sightAnimalType) && Objects.equals(locationName, sighting.locationName) && Objects.equals(rangerName, sighting.rangerName) && Objects.equals(sightAnimalAge, sighting.sightAnimalAge) && Objects.equals(sightAnimalHealth, sighting.sightAnimalHealth) && Objects.equals(sightingTime, sighting.sightingTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sightingId, sightAnimalName, sightAnimalType, locationName, rangerName, sightAnimalAge, sightAnimalHealth, sightingTime);
+    }
+
+    public int getSightingId() {
+        return sightingId;
+    }
+
+    public void setSightingId(int sightingId) {
+        this.sightingId = sightingId;
+    }
+
+    public String getSightAnimalName() {
+        return sightAnimalName;
+    }
+
+    public void setSightAnimalName(String sightAnimalName) {
+        this.sightAnimalName = sightAnimalName;
+    }
+
+    public String getSightAnimalType() {
+        return sightAnimalType;
+    }
+
+    public void setSightAnimalType(String sightAnimalType) {
+        this.sightAnimalType = sightAnimalType;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
+
+    public void setRangerName(String rangerName) {
+        this.rangerName = rangerName;
+    }
+
+    public String getSightAnimalAge() {
+        return sightAnimalAge;
+    }
+
+    public void setSightAnimalAge(String sightAnimalAge) {
+        this.sightAnimalAge = sightAnimalAge;
+    }
+
+    public String getSightAnimalHealth() {
+        return sightAnimalHealth;
+    }
+
+    public void setSightAnimalHealth(String sightAnimalHealth) {
+        this.sightAnimalHealth = sightAnimalHealth;
+    }
+
+    public Timestamp getSightingTime() {
+
+        return sightingTime;
+    }
+
+    public void setSightingTime(Timestamp sightingTime) {
+        this.sightingTime = sightingTime;
+    }
+}
