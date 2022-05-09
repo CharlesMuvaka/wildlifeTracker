268b4f78ebacf0695473732f6d1e0077c7b91f9e
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-09 23:43:43 +0300

    create a blue print that define a normal animall

---
+++ src/main/java/org/example/models/NormalAnimal.java
@@ -0,0 +1,10 @@
+package org.example.models;
+
+public class NormalAnimal extends Animal{
+
+    public NormalAnimal(String name) {
+        this.animalName = name;
+        this.animalType = "Normal Animal";
+    }
+
+}
