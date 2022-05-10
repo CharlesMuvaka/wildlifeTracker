1855765cbd0db93f69585f664d2796272e7ce3f7
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-10 05:46:44 +0300

    create tests to confirm if animal methods

---
+++ src/test/java/org/example/models/AnimalTest.java
@@ -0,0 +1,36 @@
+package org.example.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalTest {
+
+    @Test
+    void testEquals_returnsTrue() {
+        NormalAnimal one = new NormalAnimal("Name");
+        NormalAnimal two = new NormalAnimal("Name");
+
+
+        assertEquals(true, one.equals(two));
+    }
+
+
+
+    @Test
+    void getAnimalName_returrnsAnimalName() {
+        NormalAnimal one = new NormalAnimal("Name");
+        NormalAnimal two = new NormalAnimal("Name");
+
+        assertEquals("Name", one.getAnimalName());
+    }
+
+    @Test
+    void getAnimalType_returnsAnimalType() {
+        NormalAnimal one = new NormalAnimal("Name");
+        RareAnimal two = new RareAnimal("sick", "NameTwo", "Adult");
+
+        assertEquals("Normal Animal", one.getAnimalType());
+        assertEquals("Rare Animal", two.getAnimalType());
+    }
+}
