c19113d67069fb57b578c5d911d003eef4670591
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-12 10:26:47 +0300

    add remote database connection url

--- src/main/java/org/example/models/DB.java
+++ src/main/java/org/example/models/DB.java
@@ -4,7 +4,7 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-34-236-94-53.compute-1.amazonaws.com:5432/d7fba7ufgrglei", "laukiljlputcqf", "a2c1922e9fb5ab66be74fa17737423cc6fb6f6919f7a5bbe9bf19df3386de452");
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgres://ec2-34-236-94-53.compute-1.amazonaws.com:5432/d7fba7ufgrglei", "laukiljlputcqf", "a2c1922e9fb5ab66be74fa17737423cc6fb6f6919f7a5bbe9bf19df3386de452");
 
 }
