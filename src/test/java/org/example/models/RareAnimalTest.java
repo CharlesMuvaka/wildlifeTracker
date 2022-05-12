7e792aca6e9799ce2b1e2cb3eda45ebc5ed01ce9
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-12 11:12:19 +0300

    fix: remove commented code

--- src/test/java/org/example/models/RareAnimalTest.java
+++ src/test/java/org/example/models/RareAnimalTest.java
@@ -1,40 +1,40 @@
-//package org.example.models;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class RareAnimalTest {
-//
-//    @Test
-//    void testEquals_returnsTrue() {
-//        RareAnimal one = new RareAnimal("sick", "one", "young");
-//        RareAnimal two = new RareAnimal("sick", "one", "young");
-//
-//        assertEquals(one, two);
-//    }
-//
-//    @Test
-//    void testHashCode_returnsTrue() {
-//        RareAnimal one = new RareAnimal("sick", "one", "young");
-//        RareAnimal two = new RareAnimal("sick", "one", "young");
-//
-//        assertEquals(one.hashCode(), two.hashCode());
-//    }
-//
-//    @Test
-//    void getAnimalHealth_returns_animals_health() {
-//        RareAnimal one = new RareAnimal("sick", "one", "young");
-//
-//
-//        assertEquals("sick", one.getAnimalHealth());
-//    }
-//
-//    @Test
-//    void getRareAnimalAge_returns_animals_age() {
-//        RareAnimal one = new RareAnimal("sick", "one", "young");
-//
-//
-//        assertEquals("young", one.getRareAnimalAge());
-//    }
-//}
+package org.example.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RareAnimalTest {
+
+    @Test
+    void testEquals_returnsTrue() {
+        RareAnimal one = new RareAnimal("sick", "one", "young");
+        RareAnimal two = new RareAnimal("sick", "one", "young");
+
+        assertEquals(one, two);
+    }
+
+    @Test
+    void testHashCode_returnsTrue() {
+        RareAnimal one = new RareAnimal("sick", "one", "young");
+        RareAnimal two = new RareAnimal("sick", "one", "young");
+
+        assertEquals(one.hashCode(), two.hashCode());
+    }
+
+    @Test
+    void getAnimalHealth_returns_animals_health() {
+        RareAnimal one = new RareAnimal("sick", "one", "young");
+
+
+        assertEquals("sick", one.getAnimalHealth());
+    }
+
+    @Test
+    void getRareAnimalAge_returns_animals_age() {
+        RareAnimal one = new RareAnimal("sick", "one", "young");
+
+
+        assertEquals("young", one.getRareAnimalAge());
+    }
+}
--- src/test/java/org/example/models/SightDaoTest.java
+++ src/test/java/org/example/models/SightDaoTest.java
@@ -1,67 +1,67 @@
-//package org.example.models;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.sql2o.Connection;
-//import org.sql2o.Sql2o;
-//
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class SightDaoTest {
-//
-//    private  SightDao sightDao; //ignore me for now. We'll create this soon.
-//    private Connection conn;
-//
-//    Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "moringa", "Access");
-//
-//
-//    @BeforeEach
-//    public void setUp() throws Exception {
-//        sightDao = new SightDao();
-//        conn = sql2o.open();
-//    }
-//
-//    @AfterEach
-//    public void tearDown() throws Exception {
-//        try(Connection con = sql2o.open()) {
-//            String deletePersonsQuery = "DELETE FROM sightings *;";
-//            con.createQuery(deletePersonsQuery).executeUpdate();
-//        }
-//    }
-//
-//
-//    @Test
-//    void addSighting_returns_idOfAddedSight() {
-//        Sighting one  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
-//
-//        sightDao.addSighting(one);
-//        int id = one.sightingId;
-//        assertEquals(one.sightingId,id);
-//    }
-//
-//    @Test
-//     public  void getSightingById_returnsIntId() {
-//        Sighting one  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
-//
-//        sightDao.addSighting(one);
-//        int id = one.sightingId;
-//        assertEquals(one.sightingId,id);
-//    }
-//
-//    @Test
-//    void getAllSightings() {
-//
-//        Sighting one  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
-//        Sighting one1  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
-//        Sighting one2  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
-//        Sighting one3  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
-//
-//        sightDao.addSighting(one);
-//        sightDao.addSighting(one1);
-//        sightDao.addSighting(one2);
-//        sightDao.addSighting(one3);
-//        assertEquals(4, sightDao.getAllSightings().size());
-//    }
-//}
+package org.example.models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SightDaoTest {
+
+    private  SightDao sightDao; //ignore me for now. We'll create this soon.
+    private Connection conn;
+
+    Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "moringa", "Access");
+
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        sightDao = new SightDao();
+        conn = sql2o.open();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        try(Connection con = sql2o.open()) {
+            String deletePersonsQuery = "DELETE FROM sightings *;";
+            con.createQuery(deletePersonsQuery).executeUpdate();
+        }
+    }
+
+
+    @Test
+    void addSighting_returns_idOfAddedSight() {
+        Sighting one  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
+
+        sightDao.addSighting(one);
+        int id = one.sightingId;
+        assertEquals(one.sightingId,id);
+    }
+
+    @Test
+     public  void getSightingById_returnsIntId() {
+        Sighting one  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
+
+        sightDao.addSighting(one);
+        int id = one.sightingId;
+        assertEquals(one.sightingId,id);
+    }
+
+    @Test
+    void getAllSightings() {
+
+        Sighting one  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
+        Sighting one1  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
+        Sighting one2  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
+        Sighting one3  = new Sighting("Cheetah","Rare Animal","Cheche","Equatorial", " New Born ", "Sick");
+
+        sightDao.addSighting(one);
+        sightDao.addSighting(one1);
+        sightDao.addSighting(one2);
+        sightDao.addSighting(one3);
+        assertEquals(4, sightDao.getAllSightings().size());
+    }
+}
