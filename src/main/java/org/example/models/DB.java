1143fb82ee8a743eac6f53dcbf62ab4ba03c3ce7
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-05-13 13:25:44 +0300

    change database credentials

--- src/main/java/org/example/models/DB.java
+++ src/main/java/org/example/models/DB.java
@@ -4,7 +4,7 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-34-236-94-53.compute-1.amazonaws.com:5432/d7fba7ufgrglei", "laukiljlputcqf", "a2c1922e9fb5ab66be74fa17737423cc6fb6f6919f7a5bbe9bf19df3386de452");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-44-196-223-128.compute-1.amazonaws.com:5432/d8do1n1u5vmpme", "pvcywcrhlfrkcf", "1278cb11471956fabd3d4be2f590ee1bcedf6948f4ef4252bf1c1041a23c71c8");
 
 
 }
